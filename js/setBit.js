542cd1c7acc76cc77bff7a7c943e2136fbc4ac88
jtguggedal <jantore.guggedal@gmail.com>
2016-02-17 15:08:17 +0100

    .

--- js/setBit.js
+++ js/setBit.js
@@ -4,24 +4,29 @@
 //			bitOffset: 		int 0 - 7, 		eller 	char 'b' dersom hel byte skal settes 
 //											posisjon 0 er mest signifkante bit
 //			value: 			int 0 - 255
-//
-	// Setter bitmaske
+// ******
+
+	// Setter bitmaske. Satt med utgangspunkt i posisjonen lengst til venstre, den mest signifikante, som 0.
 	var bitMask = [128, 64, 32, 16, 8, 4, 2, 1];
 
 	function setBit(byteOffset, bitOffset, value) {
 		// Sjekker om det er hel byte som skal settes
 		if(bitOffset == 'b') { 
+			// Dersom byte skal, settes byttes hele den eksisterende verdien ut med den nye
 			charVal[byteOffset] = value;
 		} else {
-			// Setter enkeltbit
-			if(charVal[byteOffset] & bitMask[bitOffset]) {
-				if(value == 0) {
+			// Enkelt-bit skal settes
+			// Sjekker om den aktuelle biten allerede er satt høy
+			if(charVal[byteOffset] & bitMask[bitOffset]) { 
+				if(value == 0) { 
+					// Dersom biten er høy og ny verdi er lav, byttes denne
 					charVal[byteOffset] -= bitMask[bitOffset];
 				}
 			} else {
+					// Dersom biten er lav og ny verdi er høy, byttes den
 				 if (value == 1) {
 					charVal[byteOffset] += bitMask[bitOffset];
 				}
-			}
+			} 
 		}
-	};
+	}; 
