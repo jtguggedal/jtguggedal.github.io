66f276d74a6e3067e58627e2ed0937a3afa17d1b
jtguggedal <jantore.guggedal@gmail.com>
2016-03-08 15:20:08 +0100

    Array comparison

--- js/demo.js
+++ js/demo.js
@@ -270,7 +270,7 @@
 
             function notificationCallback(dataArray) {  
 
-                if(gameOn && (dataArray != prevNotificationArray)) {
+                if(gameOn && (dataArray.equals(prevNotificationArray))) {
 
                     if((dataArray[0] == 1 || dataArray[1] == 1 || dataArray[2] == 1 ) && (gameOn == 1)) {
                         score--;
@@ -414,5 +414,30 @@
             $('.wait-till-game').hide();
 
 
+            // Compare arrays
+            Array.prototype.equals = function (array, strict) {
+                if (!array)
+                    return false;
+
+                if (arguments.length == 1)
+                    strict = true;
+
+                if (this.length != array.length)
+                    return false;
+
+                for (var i = 0; i < this.length; i++) {
+                    if (this[i] instanceof Array && array[i] instanceof Array) {
+                        if (!this[i].equals(array[i], strict))
+                            return false;
+                    }
+                    else if (strict && this[i] != array[i]) {
+                        return false;
+                    }
+                    else if (!strict) {
+                        return this.sort().equals(array.sort(), true);
+                    }
+                }
+                return true;
+            };
 
 
