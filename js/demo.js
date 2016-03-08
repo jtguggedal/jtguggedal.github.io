8eeda1f93e90f5ce8d6ca2e123696901fe0754ae
jtguggedal <jantore.guggedal@gmail.com>
2016-03-08 15:24:03 +0100

    Compare arrays

--- js/demo.js
+++ js/demo.js
@@ -1,4 +1,31 @@
             
+
+            // Compare arrays
+            Array.prototype.equals = function (array, strict) {
+                if (!array)
+                    return false;
+
+                if (arguments.length == 1)
+                    strict = true;
+
+                if (this.length != array.length)
+                    return false;
+
+                for (var i = 0; i < this.length; i++) {
+                    if (this[i] instanceof Array && array[i] instanceof Array) {
+                        if (!this[i].equals(array[i], strict))
+                            return false;
+                    }
+                    else if (strict && this[i] != array[i]) {
+                        return false;
+                    }
+                    else if (!strict) {
+                        return this.sort().equals(array.sort(), true);
+                    }
+                }
+                return true;
+            };
+
             
             //**
             //      Gloabl settings
@@ -412,32 +439,5 @@
 
             $('.wait-till-game').css('visibility', 'visible');
             $('.wait-till-game').hide();
-
-
-            // Compare arrays
-            Array.prototype.equals = function (array, strict) {
-                if (!array)
-                    return false;
-
-                if (arguments.length == 1)
-                    strict = true;
-
-                if (this.length != array.length)
-                    return false;
-
-                for (var i = 0; i < this.length; i++) {
-                    if (this[i] instanceof Array && array[i] instanceof Array) {
-                        if (!this[i].equals(array[i], strict))
-                            return false;
-                    }
-                    else if (strict && this[i] != array[i]) {
-                        return false;
-                    }
-                    else if (!strict) {
-                        return this.sort().equals(array.sort(), true);
-                    }
-                }
-                return true;
-            };
 
 
