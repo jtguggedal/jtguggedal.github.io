3155d5383b5e65c51bf8861ce154c0b2fec9e94d
jtguggedal <jantore.guggedal@gmail.com>
2016-06-15 15:25:28 +0200

    Bug fix UUID

--- quadcopter/js/ble.js
+++ quadcopter/js/ble.js
@@ -2,10 +2,10 @@
 //      BLE connection to quadcopter
 //
 
-var serviceUUID = '6e400001-b5a3-f393-e0a9-a50e24dcca9e';
-var txCharUUID = '6e400002-b5a3-f393-e0a9-a50e24dcca9e';
-var rxCharUUID = '6e400003-b5a3-f393-e0a9-a50e24dcca9e';
-var exCharUUID = '6e400004-b5a3-f393-e0a9-a50e24dcca9e';
+var serviceUUID = '6e400001-b5a3-f393-e0a9-e50e24dcca9e';
+var txCharUUID = '6e400002-b5a3-f393-e0a9-e50e24dcca9e';
+var rxCharUUID = '6e400003-b5a3-f393-e0a9-e50e24dcca9e';
+var exCharUUID = '6e400004-b5a3-f393-e0a9-e50e24dcca9e';
 var bluetoothDevice;
 var mainServer;
 var mainService;
@@ -16,6 +16,7 @@ var txContent;
 var rxCharVal = new Uint8Array(20);
 var exCharVal = new Uint8Array(20);
 var prevTx;
+var originalPidData;
 
 // Function for connecting to quadcopter
  function connect() {
@@ -53,26 +54,32 @@ var prevTx;
         // Storing the main service object globally for easy access from other functions
         mainService = service;
         console.log('> serviceReturn: ' + service);
-        connectionStatus(1);
 
         // Get characteristics and call handler functions for both
         return Promise.all([
             service.getCharacteristic(txCharUUID)
             .then(characteristic => {
                 txChar = characteristic;
-                console.log('TX characteristic initiated');
+                console.log('TX characteristic ok');
             }),
             service.getCharacteristic(rxCharUUID)
             .then(characteristic => {
                 rxChar = characteristic
-                console.log('RX characteristic initiated');
+                console.log('RX characteristic ok');
             }),
             service.getCharacteristic(exCharUUID)
             .then(characteristic => {
                 exChar = characteristic
-                console.log('EX characteristic initiated');
+                console.log('EX characteristic ok');
             })
         ])
+        .then( () => {
+            connectionStatus(1);
+            var inputs = document.getElementsByTagName('input');
+            for(i = 0; i < inputs.length; i++){
+                inputs[i].disabled = false;
+            }
+        })
 
         // Print errors  to console
         .catch(error => {
@@ -118,4 +125,132 @@ function writeArrayToChar(char, charVal) {
             reject('No permission to write');
         }
     })
+}
+// Give input elements index corresponding to relevant bytes in characteristics
+var inputMap = [    'placeholder',
+                    '#roll-slave-p', '#roll-slave-i', '#roll-slave-d',
+                    '#pitch-slave-p', '#pitch-slave-i', '#pitch-slave-d',
+                    '#yaw-slave-p', '#yaw-slave-i', '#yaw-slave-d',
+                    '#roll-master-p', '#roll-master-i', '#roll-master-d',
+                    '#pitch-master-p', '#pitch-master-i', '#pitch-master-d',
+                    '#yaw-master-p', '#yaw-master-i', '#yaw-master-d']
+
+
+// Get PID values from quadcopter on connect
+function readPidData() {
+    /*txChar.readValue()
+    .then(originalPid => {
+
+        // Convert from dataView to Uint8Array and save original PID data for possible reset
+        originalPidData = new Uint8Array(originalPid.buffer, 0, 20);
+        console.log("Original PID data received:", originalPid);*/
+
+        // Write original PID data to input boxes
+        for(var i = 1; i <= 18; i++) {
+            select(inputMap[i]).value = exCharVal[i];
+        }
+        return originalPidData;
+    //});
+}
+
+// Reset PID values to original
+function resetPid() {
+    for(var i = 1; i <= 18; i++) {
+        select(inputMap[i]).value = originalPidData[i];
+    }
+}
+
+// Use connect() in ble.js to connect to quadcopter
+addListener('#button-connect', 'click', connect);
+
+// Send throttle and PID data to quadcopter
+addListener('#button-send', 'click', sendData);
+
+function sendData() {
+
+    // Get all PID input data and store to right index of charVal
+    for(var i = 1; i <= 18; i++) {
+        rxCharVal[i] = select(inputMap[i]).value;
+    }
+
+    // Get all control input data and store to right index of exCharVal
+    exCharVal[1] = select('#throttle').value;
+
+    // Sending PID data with rxChar over BLE
+    writeArrayToChar(rxChar, rxCharVal)
+    .then( (status) => {
+        console.log("Data sent:", rxCharVal);
+    })
+    .then( () => {
+        // Sending throttle data with exChar over BLE
+        writeArrayToChar(exChar, exCharVal)
+        .then( (status) => {
+            console.log("Data sent:", exCharVal);
+        })
+        .catch( (error) => {
+            console.log('Control data sending failed:', error)
+        });
+    })
+    .catch( (error) => {
+        console.log('PID data sending failed:', error)
+    });
+}
+
+// Roll and pitch parameters should always the same
+select('#roll-slave-p').addEventListener('input', function() {
+    select('#pitch-slave-p').value = this.value;
+});
+
+select('#roll-slave-i').addEventListener('input', function() {
+    select('#pitch-slave-i').value = this.value;
+});
+
+select('#roll-slave-d').addEventListener('input', function() {
+    select('#pitch-slave-d').value = this.value;
+});
+
+select('#pitch-slave-p').addEventListener('input', function() {
+    select('#roll-slave-p').value = this.value;
+});
+
+select('#pitch-slave-i').addEventListener('input', function() {
+    select('#roll-slave-i').value = this.value;
+});
+
+select('#pitch-slave-d').addEventListener('input', function() {
+    select('#roll-slave-d').value = this.value;
+});
+
+select('#roll-master-p').addEventListener('input', function() {
+    select('#pitch-master-p').value = this.value;
+});
+
+select('#roll-master-i').addEventListener('input', function() {
+    select('#pitch-master-i').value = this.value;
+});
+
+select('#roll-master-d').addEventListener('input', function() {
+    select('#pitch-master-d').value = this.value;
+});
+
+select('#pitch-master-p').addEventListener('input', function() {
+    select('#roll-master-p').value = this.value;
+});
+
+select('#pitch-master-i').addEventListener('input', function() {
+    select('#roll-master-i').value = this.value;
+});
+
+select('#pitch-master-d').addEventListener('input', function() {
+    select('#roll-master-d').value = this.value;
+});
+
+// Function for short-hand access to querySelector
+function select(query) {
+    return document.querySelector(query);
+}
+
+// Function for short-hand access to adding eventListener to element
+function addListener(selector, eventType, func) {
+    return document.querySelector(selector).addEventListener(eventType, func);
 }
