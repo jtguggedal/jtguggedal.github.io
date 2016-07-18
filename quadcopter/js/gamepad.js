8103f64511196f459594d0493ab89331addfed7f
jtguggedal <jantore.guggedal@gmail.com>
2016-07-18 13:41:05 +0200

    Remove css stuff

--- quadcopter/js/gamepad.js
+++ quadcopter/js/gamepad.js
@@ -16,35 +16,11 @@ function connecthandler(e) {
   addgamepad(e.gamepad);
 }
 function addgamepad(gamepad) {
-  controllers[gamepad.index] = gamepad; var d = document.createElement("div");
-  d.setAttribute("id", "controller" + gamepad.index);
-  var t = document.createElement("h1");
-  t.appendChild(document.createTextNode("gamepad: " + gamepad.id));
-  d.appendChild(t);
-  var b = document.createElement("div");
-  b.className = "buttons";
+  controllers[gamepad.index] = gamepad;
   for (var i=0; i<gamepad.buttons.length; i++) {
-    var e = document.createElement("span");
-    e.className = "button";
-    //e.id = "b" + i;
-    e.innerHTML = i;
-    b.appendChild(e);
   }
-  d.appendChild(b);
-  var a = document.createElement("div");
-  a.className = "axes";
   for (i=0; i<gamepad.axes.length; i++) {
-    e = document.createElement("progress");
-    e.className = "axis";
-    //e.id = "a" + i;
-    e.setAttribute("max", "2");
-    e.setAttribute("value", "1");
-    e.innerHTML = i;
-    a.appendChild(e);
   }
-  d.appendChild(a);
-  document.getElementById("start").style.display = "none";
-  document.body.appendChild(d);
   rAF(updateStatus);
 }
 
@@ -53,8 +29,6 @@ function disconnecthandler(e) {
 }
 
 function removegamepad(gamepad) {
-  var d = document.getElementById("controller" + gamepad.index);
-  document.body.removeChild(d);
   delete controllers[gamepad.index];
 }
 
@@ -62,8 +36,6 @@ function updateStatus() {
   scangamepads();
   for (j in controllers) {
     var controller = controllers[j];
-    var d = document.getElementById("controller" + j);
-    var buttons = d.getElementsByClassName("button");
     for (var i=0; i<controller.buttons.length; i++) {
       var b = buttons[i];
       var val = controller.buttons[i];
@@ -73,19 +45,13 @@ function updateStatus() {
         val = val.value;
       }
       var pct = Math.round(val * 100) + "%";
-      b.style.backgroundSize = pct + " " + pct;
       if (pressed) {
-        b.className = "button pressed";
       } else {
-        b.className = "button";
       }
     }
 
-    var axes = d.getElementsByClassName("axis");
     for (var i=0; i<controller.axes.length; i++) {
       var a = axes[i];
-      a.innerHTML = i + ": " + controller.axes[i].toFixed(4);
-      a.setAttribute("value", controller.axes[i] + 1);
     }
   }
   rAF(updateStatus);
