135e2c67eda9371100441fb24d06e9e553dd6c7f
jtguggedal <jantore.guggedal@gmail.com>
2016-07-18 13:30:49 +0200

    Move gamepad to index page

--- quadcopter/js/gamepad.js
+++ quadcopter/js/gamepad.js
@@ -1,4 +1,11 @@
-
+/*
+ * Gamepad API Test
+ * Written in 2013 by Ted Mielczarek <ted@mielczarek.org>
+ *
+ * To the extent possible under law, the author(s) have dedicated all copyright and related and neighboring rights to this software to the public domain worldwide. This software is distributed without any warranty.
+ *
+ * You should have received a copy of the CC0 Public Domain Dedication along with this software. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
 var haveEvents = 'GamepadEvent' in window;
 var controllers = {};
 var rAF = window.mozRequestAnimationFrame ||
@@ -9,11 +16,35 @@ function connecthandler(e) {
   addgamepad(e.gamepad);
 }
 function addgamepad(gamepad) {
+  controllers[gamepad.index] = gamepad; var d = document.createElement("div");
+  d.setAttribute("id", "controller" + gamepad.index);
+  var t = document.createElement("h1");
+  t.appendChild(document.createTextNode("gamepad: " + gamepad.id));
+  d.appendChild(t);
+  var b = document.createElement("div");
+  b.className = "buttons";
   for (var i=0; i<gamepad.buttons.length; i++) {
-
+    var e = document.createElement("span");
+    e.className = "button";
+    //e.id = "b" + i;
+    e.innerHTML = i;
+    b.appendChild(e);
   }
+  d.appendChild(b);
+  var a = document.createElement("div");
+  a.className = "axes";
   for (i=0; i<gamepad.axes.length; i++) {
+    e = document.createElement("progress");
+    e.className = "axis";
+    //e.id = "a" + i;
+    e.setAttribute("max", "2");
+    e.setAttribute("value", "1");
+    e.innerHTML = i;
+    a.appendChild(e);
   }
+  d.appendChild(a);
+  document.getElementById("start").style.display = "none";
+  document.body.appendChild(d);
   rAF(updateStatus);
 }
 
@@ -22,6 +53,8 @@ function disconnecthandler(e) {
 }
 
 function removegamepad(gamepad) {
+  var d = document.getElementById("controller" + gamepad.index);
+  document.body.removeChild(d);
   delete controllers[gamepad.index];
 }
 
@@ -29,6 +62,8 @@ function updateStatus() {
   scangamepads();
   for (j in controllers) {
     var controller = controllers[j];
+    var d = document.getElementById("controller" + j);
+    var buttons = d.getElementsByClassName("button");
     for (var i=0; i<controller.buttons.length; i++) {
       var b = buttons[i];
       var val = controller.buttons[i];
@@ -38,10 +73,19 @@ function updateStatus() {
         val = val.value;
       }
       var pct = Math.round(val * 100) + "%";
+      b.style.backgroundSize = pct + " " + pct;
+      if (pressed) {
+        b.className = "button pressed";
+      } else {
+        b.className = "button";
+      }
     }
 
+    var axes = d.getElementsByClassName("axis");
     for (var i=0; i<controller.axes.length; i++) {
-        
+      var a = axes[i];
+      a.innerHTML = i + ": " + controller.axes[i].toFixed(4);
+      a.setAttribute("value", controller.axes[i] + 1);
     }
   }
   rAF(updateStatus);
