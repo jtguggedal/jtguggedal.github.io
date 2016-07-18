d5263e3617747c759dcbfbadbdda0969bf9e60e0
jtguggedal <jantore.guggedal@gmail.com>
2016-07-18 13:24:18 +0200

    Retry gamepad

--- quadcopter/js/gamepad.js
+++ quadcopter/js/gamepad.js
@@ -1,58 +1,21 @@
 
 var haveEvents = 'GamepadEvent' in window;
 var controllers = {};
-var gamepadEnable = false;
-
 var rAF = window.mozRequestAnimationFrame ||
   window.webkitRequestAnimationFrame ||
   window.requestAnimationFrame;
 
-function startGamepad() {
-    scanGamePads();
-
+function connecthandler(e) {
+  addgamepad(e.gamepad);
 }
+function addgamepad(gamepad) {
+  for (var i=0; i<gamepad.buttons.length; i++) {
 
-function updateStatus() {
-    scanGamepads();
-
-    if(gamepadEnable) {
-      for (j in controllers) {
-        var controller = controllers[j];
-        for (var i=0; i<controller.buttons.length; i++) {
-          var b = buttons[i];
-          var val = controller.buttons[i];
-          var pressed = val == 1;
-        }
-
-        for (var i = 0; i < controller.axes.length; i++) {
-          console.log('Axis ' + i + ': ' + controller.axes[i]);
-        }
-      }
-      rAF(updateStatus);
-    }
-}
-
-function scanGamepads() {
-  var gamepads = navigator.getGamepads ? navigator.getGamepads() : (navigator.webkitGetGamepads ? navigator.webkitGetGamepads() : []);
-  for (var i = 0; i < gamepads.length; i++) {
-    if (gamepads[i]) {
-      if (!(gamepads[i].index in controllers)) {
-      } else {
-        controllers[gamepads[i].index] = gamepads[i];
-      }
-    }
   }
+  for (i=0; i<gamepad.axes.length; i++) {
+  }
+  rAF(updateStatus);
 }
-
-
-if (haveEvents) {
-  window.addEventListener("gamepadconnected", connecthandler);
-  window.addEventListener("gamepaddisconnected", disconnecthandler);
-} else {
-  setInterval(scangamepads, 500);
-}
-
-
 
 function disconnecthandler(e) {
   removegamepad(e.gamepad);
@@ -62,7 +25,44 @@ function removegamepad(gamepad) {
   delete controllers[gamepad.index];
 }
 
+function updateStatus() {
+  scangamepads();
+  for (j in controllers) {
+    var controller = controllers[j];
+    for (var i=0; i<controller.buttons.length; i++) {
+      var b = buttons[i];
+      var val = controller.buttons[i];
+      var pressed = val == 1.0;
+      if (typeof(val) == "object") {
+        pressed = val.pressed;
+        val = val.value;
+      }
+      var pct = Math.round(val * 100) + "%";
+    }
 
-function connecthandler(e) {
+    for (var i=0; i<controller.axes.length; i++) {
+        
+    }
+  }
   rAF(updateStatus);
+}
+
+function scangamepads() {
+  var gamepads = navigator.getGamepads ? navigator.getGamepads() : (navigator.webkitGetGamepads ? navigator.webkitGetGamepads() : []);
+  for (var i = 0; i < gamepads.length; i++) {
+    if (gamepads[i]) {
+      if (!(gamepads[i].index in controllers)) {
+        addgamepad(gamepads[i]);
+      } else {
+        controllers[gamepads[i].index] = gamepads[i];
+      }
+    }
+  }
+}
+
+if (haveEvents) {
+  window.addEventListener("gamepadconnected", connecthandler);
+  window.addEventListener("gamepaddisconnected", disconnecthandler);
+} else {
+  setInterval(scangamepads, 500);
 }
