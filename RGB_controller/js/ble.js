05df844e28ac49b82f419d96604e99a0bf8e6989
jtguggedal <jantore.guggedal@gmail.com>
2017-06-14 13:54:46 +0200

    Fixed error in char. UUID

--- RGB_controller/js/ble.js
+++ RGB_controller/js/ble.js
@@ -4,7 +4,7 @@ var service;
 var characteristic;
 
 const serviceUUID = "6e400001-b5a3-f393-e0a9-e50e24dcca9e";
-const charUUID = "6e400003-b5a3-f393-e0a9-e50e24dcca9e";
+const charUUID = "6e400002-b5a3-f393-e0a9-e50e24dcca9e";
 
 function connect(callback) {
     log("Scanning for devices with service UUID " + serviceUUID + "...");
