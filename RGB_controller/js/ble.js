a141c2b2bbf23ff5ecba7817b4ffb363504344eb
jtguggedal <jantore.guggedal@gmail.com>
2017-06-15 15:27:37 +0200

    Updated ignore file, minor BLE update

--- RGB_controller/js/ble.js
+++ RGB_controller/js/ble.js
@@ -4,31 +4,31 @@ var service;
 var characteristic;
 
 const serviceUUID = "6e400001-b5a3-f393-e0a9-e50e24dcca9e";
-const charUUID = "6e400002-b5a3-f393-e0a9-e50e24dcca9e";
+const characteristicUUID = "6e400002-b5a3-f393-e0a9-e50e24dcca9e";
 
 function connect(callbackOnConnect) {
-    log("Scanning for devices with service UUID " + serviceUUID + "...");
+    log("Scanning for devices with service UUID " + serviceUUID );
     navigator.bluetooth.requestDevice(
         {filters: [{services: [serviceUUID]}]}
     )
     .then( d => {
         device = d;
-        log("Found device " + d + ", trying to connect to GATT server...");
-        return d.gatt.connect();
+        log("Found device " + device + ", trying to connect to GATT server");
+        return device.gatt.connect();
     })
     .then( s => {
         server = s;
-        log("Connected to server " + s + ", getting service...");
+        log("Connected to server " + s + ", getting service");
         return server.getPrimaryService(serviceUUID);
     })
     .then( sc => {
         service = sc;
-        log("Found service " + sc + ", getting characteristic...");
-        return sc.getCharacteristic(charUUID);
+        log("Found service " + service + ", getting characteristic");
+        return service.getCharacteristic(characteristicUUID);
     })
     .then(ch => {
         characteristic = ch;
-        log("Characteristic " + ch + " found and available globally")
+        log("Characteristic " + characteristic + " found and available globally")
         if(callbackOnConnect != undefined)
             callbackOnConnect();
     })
