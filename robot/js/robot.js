b53d642074583edb27e431eeded79e1f0761d80a
jtguggedal <jantore.guggedal@gmail.com>
2017-07-27 14:29:10 +0200

    Added basic styling of robot controller buttons, which now remains hidden until connection is established

--- robot/js/robot.js
+++ robot/js/robot.js
@@ -1,8 +1,13 @@
+// This script is dependent on ble.js and visual.js
+
+
 
 const logEnabled = true;
 
 const serviceUUID = "6e400001-b5a3-f393-e0a9-e50e24dcca9e";
 const characteristicUUID = "6e400002-b5a3-f393-e0a9-e50e24dcca9e";
+
+const ROBOT_ACTION_BYTE_INDEX   = 0x01;
 
 const MOVE_HEAD_FORWARD         = 0x01;
 const MOVE_HEAD_LEFT            = 0x02;
@@ -27,70 +32,46 @@ var bleData = new Uint8Array(20);
 
 function callbackOnConnect() {
     connected = true;
+    qs("#connect-btn").style.backgroundColor = rgb(64, 143, 70);
+    qs("#content-inner-wrapper").style.display = "block";
+    fadeIn("content-inner-wrapper");
 }
 
 // Event listeners
+
 
 clickListener("#connect-btn", function() {
     connect(serviceUUID, characteristicUUID, callbackOnConnect);
 });
 
-clickListener("#robot-move-head-forward", () => {
-    bleData[0] = MOVE_HEAD_FORWARD;
-    sendData(bleData);
-});
+clickListener("#robot-move-head-forward", () => { sendRobotAction(MOVE_HEAD_FORWARD); });
 
-clickListener("#robot-move-head-left", () => {
-    bleData[0] = MOVE_HEAD_LEFT;
-    sendData(bleData);
-});
+clickListener("#robot-move-head-left", () => { sendRobotAction(MOVE_HEAD_LEFT); });
 
-clickListener("#robot-move-head-right", () => {
-    bleData[0] = MOVE_HEAD_RIGHT;
-    sendData(bleData);
-});
+clickListener("#robot-move-head-right", () => { sendRobotAction(MOVE_HEAD_RIGHT); });
 
-clickListener("#robot-move-head-side-to-side", () => {
-    bleData[0] = MOVE_HEAD_SIDE_TO_SIDE;
-    sendData(bleData);
-});
+clickListener("#robot-move-head-side-to-side", () => { sendRobotAction(MOVE_HEAD_SIDE_TO_SIDE); });
 
-clickListener("#robot-move-right-hand-up", () => {
-    bleData[0] = MOVE_RIGHT_HAND_UP;
-    sendData(bleData);
-});
+clickListener("#robot-move-right-hand-up", () => { sendRobotAction(MOVE_RIGHT_HAND_UP); });
 
-clickListener("#robot-move-right-hand-down", () => {
-    bleData[0] = MOVE_RIGHT_HAND_DOWN;
-    sendData(bleData);
-});
+clickListener("#robot-move-right-hand-down", () => { sendRobotAction(MOVE_RIGHT_HAND_DOWN);});
 
-clickListener("#robot-move-left-hand-up", () => {
-    bleData[0] = MOVE_LEFT_HAND_UP;
-    sendData(bleData);
-});
+clickListener("#robot-move-left-hand-up", () => { sendRobotAction(MOVE_LEFT_HAND_UP); });
 
-clickListener("#robot-move-left-hand-down", () => {
-    bleData[0] = MOVE_LEFT_HAND_DOWN;
-    sendData(bleData);
-});
+clickListener("#robot-move-left-hand-down", () => { sendRobotAction(MOVE_LEFT_HAND_DOWN); });
 
-clickListener("#robot-wave-hand", () => {
-    bleData[0] = WAVE_HAND;
-    sendData(bleData);
-});
+clickListener("#robot-wave-hand", () => { bleData[0] = WAVE_HAND; });
 
-clickListener("#robot-dance", () => {
-    bleData[0] = ROBOT_DANCE;
-    sendData(bleData);
-});
+clickListener("#robot-dance", () => { sendRobotAction(ROBOT_DANCE); });
 
-clickListener("#robot-cheer", () => {
-    bleData[0] = CHEER;
-    sendData(bleData);
-});
+clickListener("#robot-cheer", () => { sendRobotAction(CHEER); });
 
 // Helper functions
+
+function sendRobotAction(action) {
+    bleData[ROBOT_ACTION_BYTE_INDEX] = action;
+    return sendData(bleData);
+}
 
 function sendData(data) {
     bleBusy = true;
@@ -102,6 +83,10 @@ function sendData(data) {
     });
 }
 
+function qs(selector) {
+    return document.querySelector(selector);
+}
+
 function clickListener(el, cb) {
-    document.querySelector(el).addEventListener("click", cb);
+    qs(el).addEventListener("click", cb);
 }
---
+++ robot/js/visual.js
@@ -0,0 +1,28 @@
+// fade out
+
+function fadeOut(el){
+  el.style.opacity = 1;
+
+  (function fade() {
+    if ((el.style.opacity -= .1) < 0) {
+      el.style.display = "none";
+    } else {
+      requestAnimationFrame(fade);
+    }
+  })();
+}
+
+// fade in
+
+function fadeIn(el, display){
+  el.style.opacity = 0;
+  el.style.display = display || "block";
+
+  (function fade() {
+    var val = parseFloat(el.style.opacity);
+    if (!((val += .1) > 1)) {
+      el.style.opacity = val;
+      requestAnimationFrame(fade);
+    }
+  })();
+}
